db7f95961e7ad59356f0288552f0516722eae938
Steve Hegyes <stephen.hegyes@outlook.com>
2022-09-24 15:42:35 -0500

    week 12 assignment

---
+++ unit-test-assignment/src/main/java/TestDemo.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+
+public class TestDemo {
+
+	
+		
+		public TestDemo() {
+
+		}
+
+		public int addPositive(int a, int b) {
+			if (a > 0 && b > 0) {
+				return a + b;
+			} else {
+				throw new IllegalArgumentException("Both parameters must be positive!");
+			}
+		}
+
+		int randomNumberSquared() {
+			int randomInt = getRandomInt();
+			return (randomInt * randomInt);
+		}
+
+		int getRandomInt() {
+			Random random = new Random();
+			return random.nextInt(10) + 1;
+
+		}
+
+	
+	
+	
+}
